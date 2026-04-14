9b06bc1551771a5ddd27c4bb3afaa3370036f4be
tomiicoronel <tomascoronel86@gmail.com>
2026-04-14 14:57:52 -0300

    setup: proyecto Springboot con conexion a PostgreSQL

---
+++ src/test/java/com/estetica/estetica/EsteticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estetica.estetica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsteticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
